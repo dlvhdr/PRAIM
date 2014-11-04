4d673f5398a15c114790aad1579dab512dbc8fa0
Dvir Kehati <dvirk11@gmail.com>
2014-11-05 00:07:23 +0200

    ActionItem, ActionMetaData, PRAIMViewModel were added. DummyGUI functionality was updated.

---
+++ src/PRAIMGUI/ActionItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAIM
+{
+    class ActionItem
+    {
+        int id;
+        string imagePath;
+        ActionMetaData metaData;
+    }
+}
---
+++ src/PRAIMGUI/ActionMetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PRAIM
+{
+    public enum Priority { High, Medium, Low }
+
+    class ActionMetaData
+    {
+        int projectID;
+        Priority priority;
+        double version;
+        DateTime dateTime;
+        StringBuilder comments;
+    }
+}
