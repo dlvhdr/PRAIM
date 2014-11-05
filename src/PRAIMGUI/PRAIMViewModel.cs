5c1a5ef5f21369b2b19d3b6ad57a1dbd2070e93a
Dolev Hadar <dolevc2@gmail.com>
2014-11-05 21:34:36 +0200

    Worked on the PRAIM-GUI and added a preview image (currently shows a     mockup snapshot).     Added the save, take snapshot buttons.     Added a DataGrid to the DB tab (with mockup data).

--- src/PRAIMGUI/PRAIMViewModel.cs
+++ src/PRAIMGUI/PRAIMViewModel.cs
@@ -9,15 +9,26 @@ namespace PRAIM
     public class PRAIMViewModel
     {
         public Priority DefaultPriority { get; set; }
+        public Priority Priority { get; set; }
         public string Version { get; set; }
+        public string Comments { get; set; }
         public int ProjectID { get; set; }
+        public TimeSpan Time { get; set; }
+        public List<Priority> PossiblePriorities { get; private set; }
 
         //PRAIM constructor. Provide default values for the project under development.
         public PRAIMViewModel(int projectID, string version, Priority defaultPriority)
         {
+            PossiblePriorities = new List<Priority> { Priority.Low, Priority.Medium, Priority.High };
             this.ProjectID = projectID;
             this.DefaultPriority = defaultPriority;
+            this.Priority = DefaultPriority;
             this.Version = version;
+            
+
+            //Mockup code
+            this.Time = DateTime.Now.TimeOfDay;
+
         }
 	
 	    //open the PRAIM dialog
