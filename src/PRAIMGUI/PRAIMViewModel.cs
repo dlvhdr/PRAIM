16c8c3eb2ddf0bec94d44a932ef6349c032e2166
Dolev Hadar <dolevc2@gmail.com>
2014-11-05 15:03:45 +0200

    Added ActionItem, ActionMetaData and PRAIMViewModel.

---
+++ src/PRAIMGUI/PRAIMViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAIM
+{
+    public class PRAIMViewModel
+    {
+        public Priority DefaultPriority { get; set; }
+        public string Version { get; set; }
+        public int ProjectID { get; set; }
+
+        //PRAIM constructor. Provide default values for the project under development.
+        public PRAIMViewModel(int projectID, string version, Priority defaultPriority)
+        {
+            this.ProjectID = projectID;
+            this.DefaultPriority = defaultPriority;
+            this.Version = version;
+        }
+	
+	    //open the PRAIM dialog
+        public bool open() { return true; }
+	
+	    //Take snapshot handler
+        bool takeSnapshot() { return true; }
+	
+	    // return true if succeed & false if fail
+        bool insertActionItem(ActionItem actionItem) { return true; }
+	
+	    // return the list of images 
+       // List<ActionItem> getActionItem(ActionMetaData metaData) { }
+        
+    }
+}
