d527faa50518fbf67d07fc0e1c7fa7739dd01d04
Dvir Kehati <dvirk11@gmail.com>
2014-11-20 14:30:20 +0200

    Added the Database project with some functionatlity.

---
+++ src/PRAIMTable.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [ActionItemId] INT            NOT NULL,
+    [Priority]     INT            NULL,
+    [ProjectID]    INT            NULL,
+    [Version]      INT            NULL,
+    [DateTime]     DATETIME       NULL,
+    [Comments]     NVARCHAR (MAX) NULL,
+    [Snapshot]     IMAGE          NULL,
+    PRIMARY KEY CLUSTERED ([ActionItemId] ASC)
+);
+
