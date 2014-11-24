a5032e3b7a14763d9223dd359e35e78dff7a9e19
Dolev Hadar <dolevc2@gmail.com>
2014-11-24 22:58:24 +0200

    Started integrating the DB with PRAIM-GUI.

--- src/Common/Controls/ImageButton.xaml.cs
+++ src/Common/Controls/ImageButton.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PRAIM.Common.Controls
+namespace Common.Controls
 {
     /// <summary>
     /// Interaction logic for ImageButton.xaml
--- src/PRAIMDataBase/Program.cs
+++ src/PRAIMDataBase/Program.cs
@@ -5,55 +5,55 @@ using System.Text;
 using System.Threading.Tasks;
 using PRAIM;
 
-namespace PRAIMDataBase
+namespace PRAIMDB
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            ActionItem actionItem = new ActionItem(2);
-            PRAIMDataBase db = new PRAIMDataBase();
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        ActionItem actionItem = new ActionItem(2);
+    //        PRAIMDataBase db = new PRAIMDataBase();
 
-            ActionMetaData metaData = new ActionMetaData();
-            metaData.Priority = Priority.High;
-            metaData.ProjectID = 1;
-            metaData.Version = 1;
-            metaData.DateTime = DateTime.Now;
-            metaData.Comments = "Dolev Hadar";
-            actionItem.metaData = metaData;
+    //        ActionMetaData metaData = new ActionMetaData();
+    //        metaData.Priority = Priority.High;
+    //        metaData.ProjectID = 1;
+    //        metaData.Version = 1;
+    //        metaData.DateTime = DateTime.Now;
+    //        metaData.Comments = "Dolev Hadar";
+    //        actionItem.metaData = metaData;
 
-            bool result = db.InsertActionItem(actionItem);
-            System.Console.WriteLine("InsertActionItem result: {0}", result);
-            result = db.InsertActionItem(actionItem);
-            System.Console.WriteLine("InsertActionItem result: {0}", result);
+    //        bool result = db.InsertActionItem(actionItem);
+    //        System.Console.WriteLine("InsertActionItem result: {0}", result);
+    //        result = db.InsertActionItem(actionItem);
+    //        System.Console.WriteLine("InsertActionItem result: {0}", result);
 
-            //GetActionItems test
-            ActionMetaData metaData2 = new ActionMetaData();
-            metaData2.Priority = (Priority)(-1);//Priority.High;
-            metaData2.ProjectID = -1;
-            metaData2.Version = -1;
-            metaData2.DateTime = null;
-            metaData2.Comments = null;//"comment";
+    //        //GetActionItems test
+    //        ActionMetaData metaData2 = new ActionMetaData();
+    //        metaData2.Priority = (Priority)(-1);//Priority.High;
+    //        metaData2.ProjectID = -1;
+    //        metaData2.Version = -1;
+    //        metaData2.DateTime = null;
+    //        metaData2.Comments = null;//"comment";
 
-            List<ActionItem> list = db.GetActionItems(metaData2);
-            if (list != null)
-            {
-                foreach (ActionItem item in list)
-                {
-                    Console.WriteLine("{0}, {1} , {2}, {3} ,{4}, {5}, {6}",
-                                       item.id, item.metaData.Priority, item.metaData.ProjectID,
-                                       item.metaData.Version, item.metaData.DateTime,
-                                       item.metaData.Comments, item.snapShot);
-                }
-            }
+    //        List<ActionItem> list = db.GetActionItems(metaData2);
+    //        if (list != null)
+    //        {
+    //            foreach (ActionItem item in list)
+    //            {
+    //                Console.WriteLine("{0}, {1} , {2}, {3} ,{4}, {5}, {6}",
+    //                                   item.id, item.metaData.Priority, item.metaData.ProjectID,
+    //                                   item.metaData.Version, item.metaData.DateTime,
+    //                                   item.metaData.Comments, item.snapShot);
+    //            }
+    //        }
 
-            //DeleteActionItems test
+    //        //DeleteActionItems test
 
-            //ActionItem actionItemToDelete = list[0];//new ActionItem(1);
+    //        //ActionItem actionItemToDelete = list[0];//new ActionItem(1);
 
-            //result = db.DeleteActionItems(actionItemToDelete);
-            //System.Console.WriteLine("DeleteActionItems result: {0}", result);
+    //        //result = db.DeleteActionItems(actionItemToDelete);
+    //        //System.Console.WriteLine("DeleteActionItems result: {0}", result);
 
-        }
-    }
+    //    }
+    //}
 }
